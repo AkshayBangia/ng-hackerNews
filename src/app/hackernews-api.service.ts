da99385c4a82d26a2a80c9c669efd6455e1e47f8
phawk <phwak101@gmail.com>
2017-11-23 06:05:27 +0530

    Switched to unofficial HN api & updated observables

--- src/app/hackernews-api.service.ts
+++ src/app/hackernews-api.service.ts
@@ -4,23 +4,34 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class HackernewsApiService {
+export class HackerNewsAPIService {
   baseUrl: string;
 
   constructor(private http:Http) {
-    this.baseUrl = 'http://hacker-news.firebaseio.com/v0';
+    this.baseUrl = 'https://node-hnapi.herokuapp.com';
   }
 
 // http.get call returns an Observable of data
 // Taking the observable and then mapping it to json format
-  fetchStories(): Observable < any > {
-    return this.http.get(`${this.baseUrl}/topstories.json`)
+  fetchStories(storyType: string, page: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${storyType}?page=${page}`)
       .map(response => response.json());
   }
 
 // Second Observable subscription for each item to show their details
-  fetchItem(id:number): Observable<any> {
-    return this.http.get(`${this.baseUrl}/item/${id}.json`)
+  fetchItemContent(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/item/${id}`)
+      .map(response => response.json());
+  }
+
+// Adding new GET request to data service to fetch comments (3rd observable)
+//   fetchComments(id: number): Observable<any> {
+//   return this.http.get(`${this.baseUrl}/item/${id}`)
+//       .map(response => response.json());
+// }
+
+  fetchUser(id: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/user/${id}`)
       .map(response => response.json());
   }
 
