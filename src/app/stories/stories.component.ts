695b62e11c72e933e291a069293ce29391404b43
phawk <phwak101@gmail.com>
2017-11-28 08:02:18 +0530

    Imported router service & refactored API parts

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
 
-import {HackernewsApiService} from '../hackernews-api.service';
+import { HackerNewsAPIService } from '../hackernews-api.service';
 
 @Component({
   selector: 'app-stories',
@@ -10,18 +11,36 @@ import {HackernewsApiService} from '../hackernews-api.service';
 })
 
 export class StoriesComponent implements OnInit {
+  typeSub: any;
+  pageSub: any;
   items;
+  storiesType;
+  pageNum: number;
+  listStart: number;
 
-  constructor(private _hackerNewsAPIService: HackernewsApiService) {}
+  constructor(
+    private _hackerNewsAPIService: HackerNewsAPIService,
+    private route: ActivatedRoute) {}
 
-// Hook fires when the component is initialized
-// we subscribe to the data stream and set the items attribute to what gets returned
+// Service "ActivatedRoute" allows to access information in the route
+// Then hook fires when the component is initialized
+// we subscribe to the route data property and store "storiesType" into a component variable
   ngOnInit() {
-  this._hackerNewsAPIService.fetchStories()
-      .subscribe(
-        items => this.items = items,
-        error => console.log('Error fetching stories')
-      );
+    this.typeSub = this.route
+      .data
+      .subscribe(data => this.storiesType = (data as any).storiesType);
+    // New API doesn't load all top 500 stories
+    // we need to add page number as an argument
+    // starting with passing 'news' and page number 1
+    this.pageSub = this.route.params.subscribe(params => {
+      this.pageNum = params['page']? +params['page']: 1;
+      this._hackerNewsAPIService.fetchStories(this.storiesType, this.pageNum)
+        .subscribe(
+          items => this.items = items,
+          error => console.log('Error fetching' + this.storiesType + 'stories'),
+          () => {this.listStart = ((this.pageNum - 1) * 30) + 1;
+          window.scrollTo(0,0);
+        });
+    });
   }
-
 }
